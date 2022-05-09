5c04550b9aae6393be8afad627a1975e63b55911
FatimaGloria <janettgloria1@gmail.com>
2022-05-08 22:05:16 -0500

    4.2 Se modifica codigo de la prueba para que coincida con el tema

--- spiderverse_app/test/spiderman.test.js
+++ spiderverse_app/test/spiderman.test.js
@@ -1,6 +1,15 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+describe("Unit Tests dor Spiderman class", () => {
+    test('1) Create a spiderman object', () => {
+      
+    //Código para instanciar un objeto Spiderman con esta información
+      const andrewGarfield = new Spiderman("The Amazing Spiderman ", 31, "Andrew Garfield", 2, "Sony")
+     
+    // Validamos que el código funcione de la forma esperada
+      expect(andrewGarfield.name).toBe("The Amazing Spiderman");
+      expect(andrewGarfield.age).toBe(31);
+      expect(andrewGarfield.actor).toBe("Andrew Garfield");
+      expect(andrewGarfield.movies).toBe(2);
+      expect(andrewGarfield.studio).toBe("Sony");  
     });
+
   })
