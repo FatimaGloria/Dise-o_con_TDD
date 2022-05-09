b5dea6bff876d6342ae95167f9d8c6e4ad25bc8c
FatimaGloria <janettgloria1@gmail.com>
2022-05-08 22:31:20 -0500

    **Se corrige error de sintaxis

--- spiderverse_app/test/spiderman.test.js
+++ spiderverse_app/test/spiderman.test.js
@@ -4,7 +4,7 @@ describe("Unit Tests dor Spiderman class", () => {
     test('1) Create a spiderman object', () => {
       
     //Código para instanciar un objeto Spiderman con esta información
-      const andrewGarfield = new Spiderman("The Amazing Spiderman ", 31, "Andrew Garfield", 2, "Sony")
+      const andrewGarfield = new Spiderman("The Amazing Spiderman", 31, "Andrew Garfield", 2, "Sony")
      
     // Validamos que el código funcione de la forma esperada
       expect(andrewGarfield.name).toBe("The Amazing Spiderman");
